d23b1a9b26cfdadc4670d8ca9ad3502716ad4778
Dmitriy Melnik <melniknc@gmail.com>
2019-12-14 21:48:39 +0200

    adding data to movies_2019.sql

--- hw12/script.sql
+++ hw12/script.sql
@@ -1,34 +1,36 @@
 CREATE TABLE movies(
-    id integer NOT NULL PRIMARY KEY,
+    id serial NOT NULL PRIMARY KEY,
     name text NOT NULL,
     release_date date NOT NULL,
     budget integer NOT NULL
 );
 
 CREATE TABLE directors(
-    id integer NOT NULL PRIMARY KEY,
-    name text NOT NULL,
+    id serial NOT NULL PRIMARY KEY,
+    first_name text NOT NULL,
+    last_name text NOT NULL,
     born_date date NOT NULL,
     total_movies smallint NOT NULL
 );
 
 CREATE TABLE actors(
-    id integer NOT NULL PRIMARY KEY,
-    name text NOT NULL,
+    id serial NOT NULL PRIMARY KEY,
+    first_name text NOT NULL,
+    last_name text NOT NULL,
     born_date date NOT NULL,
     total_movies smallint NOT NULL
 );
 
 CREATE TABLE directors_movies(
-    movie_id integer,
+    movie_id serial,
     FOREIGN KEY (movie_id) REFERENCES movies(id),
-    director_id integer,
+    director_id serial,
     FOREIGN KEY(director_id) REFERENCES directors(id)
 );
 
 CREATE TABLE actors_movies(
-    movie_id integer,
+    movie_id serial,
     FOREIGN KEY (movie_id) REFERENCES movies(id),
-    actor_id integer,
+    actor_id serial,
     FOREIGN KEY (actor_id) REFERENCES actors(id)
 );
